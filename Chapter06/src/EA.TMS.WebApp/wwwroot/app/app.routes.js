7d9a56b4abbaeb080e58405d79e2697410377144
hitmanharkness <hitmanharkness@hotmail.com>
2018-02-04 12:40:30 -0700

    Chapters 3,6

---
+++ Chapter06/src/EA.TMS.WebApp/wwwroot/app/app.routes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+var service_request_component_1 = require("./components/tenant/service-request.component");
+var menu_component_1 = require("./menu.component");
+//Routes Configuration
+exports.routes = [
+    { path: 'createServiceRequest', component: service_request_component_1.ServiceRequestComponent },
+    { path: 'menu', component: menu_component_1.MenuComponent },
+    {
+        path: '',
+        redirectTo: '/menu',
+        pathMatch: 'full'
+    },
+];
+exports.routing = router_1.RouterModule.forRoot(exports.routes);
+//# sourceMappingURL=app.routes.js.map
---
+++ Chapter06/src/EA.TMS.WebApp/wwwroot/app/shared/models/data-models.interface.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ServiceRequest = (function () {
+    function ServiceRequest() {
+    }
+    return ServiceRequest;
+}());
+exports.ServiceRequest = ServiceRequest;
+//# sourceMappingURL=data-models.interface.js.map
